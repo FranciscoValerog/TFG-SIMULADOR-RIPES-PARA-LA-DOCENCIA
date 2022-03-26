0b2da8c736f034c8e2215b5653ce2cfeb3f3329f
FranciscoValerog <fran.valgil@gmail.com>
2022-03-26 17:16:36 +0100

    Rorganizacion
    Añadimos de forma externa los repositorios necesarios

--- src/fonts.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Ripes {
-namespace Fonts {
-
-constexpr auto monospace = "Inconsolata";
-
-}  // namespace Fonts
-}  // namespace Ripes
--- src/utilities/systemutils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QStringList>
-
-namespace Ripes {
-
-bool isExecutable(const QString& path, const QStringList& dummyArgs = {});
-
-}  // namespace Ripes
